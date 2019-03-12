e520ed8311b51cb714b79ac959b8a344b1f38786
shantigilbert <shantic@gmail.com>
2019-03-11 23:30:11 -0600

    ES: bump to f9e4889

--- packages/sx05re/emulationstation/package.mk
+++ packages/sx05re/emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present CoreELEC (https://coreelec.org)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="70da690ca457d110fea78e46313e8049a60fd675"
+PKG_VERSION="f9e48897d3f8a5630496453a7a1ecbf9dbf98a14"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
